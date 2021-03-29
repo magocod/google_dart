128abb93fded070cbc5bd63cddcb9f9b86d7c8ac
magocod <magoyeison@gmail.com>
2021-03-29 18:49:46 -0400

    update first app

--- mobile/startup_namer/lib/main.dart
+++ mobile/startup_namer/lib/main.dart
@@ -19,6 +19,9 @@ class MyApp extends StatelessWidget {
       //     child: RandomWords(),
       //   ),
       // ),
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.indigo,
+      ),
       home: RandomWords(),
     );
   }
@@ -32,15 +35,26 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   // Add the next two lines.
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
     // final WordPair wordPair = WordPair.random();
     // return Text(wordPair.asPascalCase);
-    return Scaffold (                   // Add from here...
+    // return Scaffold (
+    //   appBar: AppBar(
+    //     title: Text('Startup Name Generator'),
+    //   ),
+    //   body: _buildSuggestions(),
+    // );
+    return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ], // ... to here.
       ),
       body: _buildSuggestions(),
     );
@@ -76,15 +90,61 @@ class RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-        }
-    );
+        });
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
       ),
     );
   }
